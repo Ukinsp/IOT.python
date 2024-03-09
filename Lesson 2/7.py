6911ebb47d474b4ccf45ffed69c12c3042b4467a
Ukinsp <xenios.samara@gmail.com>
2024-03-09 14:55:05 +0400

    09.03

--- Lesson 2/7.py
+++ Lesson 2/7.py
@@ -1,5 +1,16 @@
 import numpy as np
-a = [3, 2, 5, 4, 1, 7]
-x = np.min(a)
-print(x)
+a = np.array([3, 2, 5, 4, 1, 7])
+
+for i in range(3):
+    x = np.argmin(a)
+    print(x)
+    a[x]=np.max(a)
+
+# x = np.argmin(a)
+# print(x)
+# a[x]=np.max(a)
+#
+# x = np.argmin(a)
+# print(x)
+# a[x]=np.max(a)
 
---
+++ Lesson1/14.py
@@ -0,0 +1,5 @@
+if 10 > 100:
+    print('yes')
+else:
+    print('nope')
+    
